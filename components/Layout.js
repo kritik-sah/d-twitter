6b1ec62d924ae20bb1ce76aa1fbbd3eabe4d3cab
Kritik Sah <kritiksah0@gmail.com>
2022-06-04 01:49:37 +0530

    build issue solved

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Rightbar from './Rightbar'
 import Sidebar from './Sidebar'
-import Styles from "../styles/home.module.css"
 import { useMoralis } from 'react-moralis'
 import { Icon, ConnectButton } from 'web3uikit';
 
